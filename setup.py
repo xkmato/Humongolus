cb5154594b9cf12655107a2e2193f82f76a30762
Kenneth Matovu <kbonky@gmail.com>
2015-07-27 16:14:05 +0300

    patch setup file

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@
 from distutils.core import setup
 from setuptools import find_packages
 
-setup(name='Humongolus',
+setup(name='Humongolus-patch',
       version='1.0.6',
       author='Christopher Coté',
       packages=find_packages(),
